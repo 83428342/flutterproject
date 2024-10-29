6e064247fe6f90d71561a61730db7cf6ac336fd2
중앙대22 이성욱 <99406222+83428342@users.noreply.github.com>
2024-10-30 00:48:14 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -21,27 +21,91 @@ class MainRoom extends StatefulWidget {
 }
 
 class _MainRoomState extends State<MainRoom> {
+  final List<String> chattingRooms = [];
+
+  void addChattingRoom(String roomName) {
+    setState(() {
+      chattingRooms.add(roomName);
+    });
+  }
+
+  void chattingRoomDialog() {
+    String roomName = '';
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Agent Name'),
+            content: TextField(
+              onChanged: (value) {
+                roomName = value;
+              },
+              decoration:
+                  const InputDecoration(hintText: 'Write down new name.'),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  if (roomName.isNotEmpty) {
+                    addChattingRoom(roomName);
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: const Text('Apply'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chatting'),
-        actions: const [
-          IconButton(
+        actions: [
+          const IconButton(
             onPressed: null,
             icon: Icon(Icons.search),
           ),
           IconButton(
-            onPressed: null,
-            icon: Icon(Icons.chat_bubble_outline),
+            onPressed: chattingRoomDialog,
+            icon: const Icon(Icons.chat_bubble_outline),
           ),
-          IconButton(
+          const IconButton(
             onPressed: null,
             icon: Icon(Icons.settings_outlined),
           ),
         ],
       ),
-      body: ListView(),
+      body: ListView.builder(
+        itemCount: chattingRooms.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(chattingRooms[index]),
+            leading: const Icon(
+              Icons.person,
+              size: 40,
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ChattingScreen(roomName: chattingRooms[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.person),
@@ -53,5 +117,14 @@ class _MainRoomState extends State<MainRoom> {
         ),
       ]),
     );
+  }
+}
+
+class ChattingScreen extends StatelessWidget {
+  const ChattingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
